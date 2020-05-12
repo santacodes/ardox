b3d00bff02bf6130f7f1c2c429ceaf29f404546d
Santhosh Sundaram <santa@localhost.localdomain>
2020-05-12 15:28:59 +0530

    invites

--- Ardox.py
+++ Ardox.py
@@ -185,7 +185,10 @@ async def on_message(message):
         
         elif message.content.startswith(prefix+'announce'): #date hour-minute
             pass #threading
-            
+        
+        elif message.content.find('discord.gg'):
+            message.channel.purge(1)
+
         conti(message)
         print(names,count)
         print(top_ten_names,top_ten_count)
