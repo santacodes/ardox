d9b94629c7e909da6afd6ad41aba976e297f7130
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2020-05-23 19:22:06 +0530

    fixexxxxx

--- Ardox.py
+++ Ardox.py
@@ -247,7 +247,7 @@ async def on_message(message):
         elif str(message.channel.category).lower() == 'art':
             if message.content.find('http') != -1:
                 await message.add_reaction('👍')
-            elif message == discord.Attachments:
+            elif message is discord.Attachment:
                 await message.add_reaction('👍')
             conti(message)
     
