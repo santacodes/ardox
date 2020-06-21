40b72f5875084b2d01672bb825b5b6d88a2caa30
Santhosh <santhoshsundaram9650@gmail.com>
2020-06-21 15:38:37 +0530

    lol im mad bruh

--- Ardox.py
+++ Ardox.py
@@ -170,7 +170,7 @@ async def kick(ctx,member : discord.Member,*,reason = None):
     if ctx.author.top_role in premium_user_roles: await member.kick(reason = reason)
     mod_logs_channel = ctx.guild.get_channel(713074242543157388)
     kick_embed = discord.Embed(title = str(member) + 'Got kicked due to the following reason - ' + reason, colour = discord.Color.red())
-    ctx.message.channel.send(embed = kick_embed)
+    mod_logs_channel.send(embed = kick_embed)
 
 @client.command()
 async def ban(ctx,member : discord.Member,*,reason = None):
